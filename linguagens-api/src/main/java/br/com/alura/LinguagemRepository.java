1122522dff056ec2a0872548438f80bbcde7c44f
Aline Ferreira <aline.fn80@gmail.com>
2022-07-24 16:42:15 -0300

    separando repositorios alura stickers e linguagens api

--- linguagens-api/src/main/java/br/com/alura/LinguagemRepository.java
+++
@@ -1,8 +0,0 @@
-package br.com.alura;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface LinguagemRepository extends MongoRepository<Linguagem, String>{
-
-	void deleteOne(String  id);
-}
